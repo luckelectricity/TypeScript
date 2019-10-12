d238cbe07c44d311f01f587c6388463a9747c54e
luckelectricity <luckelectricity@outlook.com>
2019-10-12 18:06:11 +0800

    feat 增加部分代码

--- client-side/src/index.ts
+++ client-side/src/index.ts
@@ -1,6 +1,17 @@
-let a: number = 123;
+import { KeyLike } from "crypto";
 
+// number 类型
+const a: number = 123;
+// string 类型
+const str: string = "kele";
+
+// let kele: "kele"; 它们本身并不是很实用，但是可以在一个联合类型中组合创建一个强大的（实用的）抽象 其实我也不懂这句话啥意思 以后在看吧
+
+ // kele = "123"; 不能重复定义 TS2454: Variable 'kele' is used before being assigned.
+const list1: number[] = [a, 456, 789]; // 推荐写法
+// const list2: Array<number> = [123, 456, 789]; 不推荐 会自动转换成上面那种写法
+const bool: boolean = true;
 const h1 = document.createElement("h1");
 
-h1.innerHTML = "Hello, I am kele";
+h1.innerHTML = "Hello, I am " + str + " " + a;
 document.body.appendChild(h1);
