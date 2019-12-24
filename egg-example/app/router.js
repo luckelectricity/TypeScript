9625afa0dc46e99e5529debc43e1524579ddfff7
luckelectricity <luckelectricity@outlook.com>
2019-12-24 18:31:21 +0800

    feat 1

--- egg-example/app/router.js
+++ egg-example/app/router.js
@@ -7,4 +7,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.get('/home/:id', controller.home.page);
+  router.get('/news', controller.news.list)
 };
